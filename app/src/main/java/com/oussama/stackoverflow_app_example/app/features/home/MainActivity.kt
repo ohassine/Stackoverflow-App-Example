6a94a08b128e0e79c472f34c8743fb5b1f3ea428
ohassine <ohassine@mobelite.fr>
2020-12-01 15:24:51 +0100

    create domain and entities modules

--- app/src/main/java/com/oussama/stackoverflow_app_example/app/features/home/MainActivity.kt
+++ app/src/main/java/com/oussama/stackoverflow_app_example/app/features/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.oussama.stackoverflow_app_example
+package com.oussama.stackoverflow_app_example.app.features.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oussama.stackoverflow_app_example.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
